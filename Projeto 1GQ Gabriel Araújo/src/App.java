7340c903a19043b8c4aa4b1d29852e93bf8fea51
Gabriel Araújo <142544309+Gabriel-SL-Araujo@users.noreply.github.com>
2025-04-03 06:16:08 -0300

    Update App.java

--- Projeto 1GQ Gabriel Araújo/src/App.java
+++ Projeto 1GQ Gabriel Araújo/src/App.java
@@ -1,27 +1,69 @@
 import AVLTreeGenerica.AVLTree;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        AVLTree<Integer> numbers = new AVLTree<>();
+    public static void main(String[] args){
         
-        numbers.insert(2);
+        String textBlock = """
+                0 • Sair do Programa;
+                1 • Adicionar inteiro à árvore;
+                2 • Passear em Ordem pela árvore;
+                3 • Passear por Nível pela árvore;
+                4 • Remover inteiro da árvore; 
+                """; // Exibe os comandos possíveis
 
-        numbers.insert(1);
-        numbers.insert(4);
-        numbers.insert(5);
-        numbers.insert(9);
-        numbers.insert(3);
-        numbers.insert(6);
-        numbers.insert(7);
+        int option; 
+        Scanner scan = new Scanner(System.in);
+        AVLTree<Integer> treeOfIntegers = new AVLTree<>(); // Criação da árvore
+        do { 
+            System.out.println(textBlock);
+            option = scan.nextInt();
+            scan.nextLine();
+            acessarMenuDeOpcoes(option, scan, treeOfIntegers); // Função que acessa as opções e efetua as interações com a árvore
+        } while (option != 0);
 
-        System.out.println("Passeio em ordem: ");
+        scan.close();
+    
+    }
+        
+    private static void acessarMenuDeOpcoes(int option, Scanner scan, AVLTree<Integer> treeOfIntegers){
+        switch(option){
+            case 0 ->{ // Sair do programa
+                System.out.println("Fechando aplicação...");
+               }
+                
+            case 1 ->{ // Inserir um nó Integer na árvore
+                System.out.println("Digite um número: ");
+                int newNode = scan.nextInt();
+                scan.nextLine();
+                treeOfIntegers.insert(newNode);
+                System.out.println("Valor " + newNode + " foi inserido.");
+                System.out.println();
+            }
 
-        numbers.passeioEmOrdem();
+            case 2 ->{ // Passear em ordem pela árvore
+                System.out.println("Passeio em Ordem na árvore: ");
+                treeOfIntegers.passeioEmOrdem();
+                System.out.println();
+            }
 
-        System.out.println("Passeio por nível: \n");
+            case 3 ->{ // Passear por nível na árvore
+                System.out.println("Passeio por Nível na árvore: ");
+                treeOfIntegers.passeioPorNivel();
+                System.out.println();
+            }
 
-        numbers.passeioPorNivel();
+            case 4 ->{ // Remover um nó Integer da árvore
+                System.out.println("Digite o inteiro que queira remover da árvore: ");
+                int nodeToBeRemoved = scan.nextInt();
+                scan.nextLine();
+                treeOfIntegers.remove(nodeToBeRemoved);
+                System.out.println();
+            }
 
-
+            default ->{ // Comandos inválidos serõ desconsiderados
+                System.out.println("Comando não reconhecido. Tente novamente.");
+            }
+        }
     }
 }
